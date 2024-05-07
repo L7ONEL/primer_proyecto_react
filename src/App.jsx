b27ac796d45548c696856e8921df157f62d111d8
L7ONEL <lionelardusso4730@gmail.com>
2024-05-07 19:21:04 -0300

    se agregó los archivos de la carpeta de REACT

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+import Contador from './componentes/Contador'
+import './App.css'
+
+export default class App extends Component{
+
+  render() {
+    return(
+      <div className='ListaContadores'>
+        <Contador>Alumnos</Contador>
+        <Contador>Profesores</Contador>
+        <Contador>Aulas</Contador>
+      </div>
+    )
+  }
+
+}
